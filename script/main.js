23392edbd6c8a32eee6c2edfc361e6c8d5ce90c5
Fabio Gadau <fabiogadau92@gmail.com>
2020-03-26 14:30:36 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,11 @@
+console.log('JS is working!');
+
+// 1. Get how many km the user wants to travel.
+
+// 2. Get the user's age.
+
+// 3. With this information calculate the ticket price.
+
+// 4. Apply a 20% discount for minors and a 40% discount for over 65s.
+
+// 5. Show the price on the page, indicating whether a discount has been applied.
